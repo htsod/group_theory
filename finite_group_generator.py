de2ea9e0fc26844ccfce5aed80e9335fec4be68b
dio <tianmingliang@ucsb.edu>
2023-01-21 00:26:17 -0800

    added comment
    More description to the code

--- finite_group_generator.py
+++ finite_group_generator.py
@@ -6,25 +6,33 @@
 # Using nodal network to model the branching of multiplication table
 # Using recursion to generate the full branches
 
-    
+# node function as
 class node:
     def __init__(self, n):
+        """
+        :param n: the dimensions of the group
+        """
+        # self.item is being used to hold the multiplication table
         self.item = [[[[-1 for i in range(n)] for j in range(n)]]]
         self.n = n
         self.branches = 1
         self.gen = 0
+        # indices i, j is being used to track and modify the element in the mult table
         self.i = 0
         self.j = 0
         self.state = True
 
 
     def get_stat(self):
-        """Print the state of the object node"""
+        """Print the state of the object node, is being used for debug purposes"""
         print(f"the current generation is {self.gen + 1} \nwith total {self.branches} branches \nthe indexes are ({self.i}, {self.j})")
 
 
-    def update(self):
-        """Update i, j index"""
+    def _update(self):
+        """
+        Update i, j index if the state of the system is false.
+        The state will turn false if certain conditions are met
+        """
         if self.j < (self.n - 1):
             self.j += 1
         elif self.i < (self.n - 1):
@@ -34,24 +42,40 @@ class node:
             self.state = False
 
 
-    def cond(self, mult_list, branch):
-        """Produce restricted list of possible transformation between group member by enforcing the once_and_only_once rule"""
-        table = self.item[self.gen][branch-1]  
+    def _cond(self, mult_list, branch):
+        """
+        Description:
+            Produce restricted list of possible transformation between group member
+            Enforcing the once_and_only_once rule
+
+        :param mult_list: Possible ways to assign the element in the mult table
+        :param branch: The current branch
+        :return: Mult table without any duplicate
+        """
+        # create a link to self.item with the current generation and branch
+        table = self.item[self.gen][branch-1]
+        # create x and y row to iterate through
         x_row = table[self.i]
         y_row = [ele[self.j] for ele in table]
         del_list = []
+        # Remove any duplicate element in mult_list
         for i in mult_list:
             if (i in x_row) or (i in y_row):
                 del_list.append(i)
         for i in del_list:
-            mult_list.remove(i) 
+            mult_list.remove(i)
         return mult_list
 
 
-    def mult(self):
-        """Generate branches for index i, j and update stats to next generation"""
+    def _mult(self):
+        """
+        Description:
+            Generate branches for index i, j and update stats to next generation
+        """
         next_gen = []
         mult_list = []
+
+        # if element either i or j is the identity, then fill the current spot with j or i.
         if self.i == 0:
             mult_list.append(self.j)
             for b in range(self.branches):
@@ -67,12 +91,15 @@ class node:
                 clone[self.i][self.j] = mult_list[0]
                 next_gen.append(clone)
             self.branches = self.branches
-            
+
+        #
         else:
             new_branch = 0
             for b in range(self.branches):
+                # full_list gives all the possibilities while mult_list gives the refined list
                 full_list = [i for i in range(self.n)]
-                mult_list = self.cond(full_list, b)
+                mult_list = self._cond(full_list, b)
+                # for each possible possibilities, generate add a new branch
                 for p in mult_list:
                     clone = [ele[:] for ele in self.item[self.gen][b-1]]
                     clone[self.i][self.j] = p
@@ -80,7 +107,7 @@ class node:
                     new_branch += 1
             self.branches = new_branch
             
-        self.update()
+        self._update()
         self.gen += 1
         return next_gen
 
@@ -88,14 +115,15 @@ class node:
     def edge(self):
         """Runs self.edge() recursively until i, j indexes are out of bound"""
         self.get_stat()
-        self.item.append(self.mult())
+        self.item.append(self._mult())
+        print(self.state)
         if self.state == False:
-            return self
+            return self.item
         else:
             return self.edge()
 
 
 dim = int(input("Enter an positive integer: "))
 g = node(dim)
-g.edge()
-
+mult_table = g.edge()
+print(mult_table)
